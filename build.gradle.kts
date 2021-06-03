aac9bab884de367d841c27ae3f981929ec75ef9d
Ryan Nett <JNett96@gmail.com>
2021-06-02 18:48:56 -0700

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins{
 allprojects{
 
     group = "com.github.rnett.ktjs-github-action"
-    version = "1.2.0-SNAPSHOT"
+    version = "1.2.1-SNAPSHOT"
 
     repositories {
         mavenCentral()
