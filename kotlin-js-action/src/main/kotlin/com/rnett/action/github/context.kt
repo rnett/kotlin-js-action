fb2d48ed423ee82182c0549a0ba1e0fb954e17b8
Ryan Nett <JNett96@gmail.com>
2021-06-03 16:58:25 -0700

    Only hash things under workspace by default

--- kotlin-js-action/src/main/kotlin/com/rnett/action/github/context.kt
+++ kotlin-js-action/src/main/kotlin/com/rnett/action/github/context.kt
@@ -1,6 +1,7 @@
 package com.rnett.action.github
 
 import com.rnett.action.JsObject
+import com.rnett.action.Path
 import com.rnett.action.core.env
 import com.rnett.action.glob.globFlow
 import kotlinx.coroutines.asDeferred
@@ -17,12 +18,15 @@ public object github {
         public val action: String by env("GITHUB_ACTION")
         public val actor: String by env("GITHUB_ACTOR")
         public val job: String by env("GITHUB_JOB")
+        public val workspace: String by env("GITHUB_WORKSPACE")
+        public val workspacePath: Path get() = Path(workspace)
 
-        public suspend fun hashFiles(patterns: List<String>): String {
+        public suspend fun hashFiles(patterns: List<String>, underWorkspace: Boolean = true): String {
             val result = crypto.createHash("sha256")
+            val myWorkspacePath = workspacePath
             globFlow(patterns)
                 .collect {
-                    if (it.isFile) {
+                    if (it.isFile && (!underWorkspace || it.isDescendantOf(myWorkspacePath))) {
                         val hash = crypto.createHash("sha256")
                         val read = fs.createReadStream(it.path, JsObject<fs.`T$50`> { })
 
