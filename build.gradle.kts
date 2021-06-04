a86f91a9539b6480b01d48dcc4c37002b7c76d18
Ryan Nett <JNett96@gmail.com>
2021-06-04 12:33:26 -0700

    Try new release action

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,14 @@
-plugins{
+plugins {
     kotlin("js") version "1.5.10" apply false
     kotlin("jvm") version "1.5.10" apply false
     id("com.vanniktech.maven.publish") version "0.15.1" apply false
     id("org.jetbrains.dokka") version "1.4.32" apply false
 }
 
-allprojects{
+allprojects {
 
     group = "com.github.rnett.ktjs-github-action"
-    version = "1.2.1-SNAPSHOT"
+    version = "1.2.1-RC"
 
     repositories {
         mavenCentral()
