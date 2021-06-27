612e4eb2a86660de7bdd381fa0e988c5eb74da01
Ryan Nett <JNett96@gmail.com>
2021-06-27 15:14:26 -0700

    Try RC for publishing fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 allprojects {
     group = "com.github.rnett.ktjs-github-action"
-    version = "1.4.0-SNAPSHOT"
+    version = "1.4.0-RC"
 
     repositories {
         mavenCentral()
